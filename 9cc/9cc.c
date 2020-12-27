ae1af496cf377f78fe5ab02a976e82eb638a794d
togo-yamanaka <togo.yamanaka@ariseanalytics.com>
2020-12-27 15:27:29 +0000

    足し・引き算が出来るようになった

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -2,17 +2,43 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv)
+// argc -> コマンドも含めた引数の個数
+// **argv -> コマンドを文字列としたリスト
 {
+    // 引数が1個以外のときにエラーにする
     if (argc != 2)
     {
         fprintf(stderr, "引数の個数が正しくありません");
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+    printf("    mov rax, %d\n", strtol(p, &p, 10));
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-')
+        {
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
+    }
+
     printf("    ret\n");
 
     return 0;
